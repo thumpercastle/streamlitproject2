7d1cfb53c1e3ad1c7af5413a4066ea3bb9a8b005
thumpercastle <tony.ryb@gmail.com>
2025-11-24 13:21:03 +0000

    updated `key` parameters in `main.py` for `dataframe` and `plotly_chart` components to improve dynamic state referencing in value counts visualization

--- src/streamlitproject2/main.py
+++ src/streamlitproject2/main.py
@@ -476,11 +476,11 @@ def page_2():
             # TODO: Enable value counts for other parameters
             # counts = pd.DataFrame([survey.counts().loc[name]["Daytime"], survey.counts().loc[name]["Night-time"]]).T
             # counts = survey.counts()
-            st.dataframe(ss["counts"].loc[name], key="counts_df", width="stretch")
+            st.dataframe(ss["counts"].loc[name], key=f"counts_df_{name}", width="stretch")
 
             st.markdown(f"## {name} L90 value counts")
             fig = ss["counts"].loc[name].plot.bar(facet_row="variable")
-            st.plotly_chart(fig, key="counts_bar", config={
+            st.plotly_chart(fig, key=f"counts_bar_{name}", config={
                 "y": "Occurrences",
                 "x": "dB",
                 "color": "Period",
